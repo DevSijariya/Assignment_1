35c3ac8db0044841f0fda615418118184c1059b8
Sanskar Sijariya <129308556+DevSijariya@users.noreply.github.com>
2023-08-04 18:26:51 +0530

    Update Answer5.py

--- Answer5.py
+++ Answer5.py
@@ -1,13 +1,13 @@
 def calcTotalTime(path):
  
-    # Stores total time
+  
     time = 0
  
-    # Initial position
+
     x = 0
     y = 0
  
-    # Stores visited segments
+  
     s = set([])
  
     for i in range(len(path)):
@@ -27,25 +27,22 @@ def calcTotalTime(path):
         elif (path[i] == 'W'):
             x -= 1
  
-        # Check whether segment
-        # is present in the set
+      
         if (p + x, q + y) not in s:
-            # Increment the value
-            # of time by 2
+           
             time += 2
  
-            # Insert segment into the set
+          
             s.add((p + x, q + y))
  
         else:
             time += 1
  
-    # Print the value
-    # of time
+    
     print(time)
  
  
-# Driver Code
+
 if __name__ == "__main__":
  
     path = "NSE"
